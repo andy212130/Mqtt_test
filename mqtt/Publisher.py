a4da6f4d2a9e0c54a487ec0fcd31448f7f1e88f5
andy212130 <quan_xi87@yahoo.com.tw>
2021-06-04 08:13:09 +0800

    new version 1.0.1

--- mqtt/Publisher.py
+++ mqtt/Publisher.py
@@ -1,10 +1,12 @@
 # Publisher.py
 import paho.mqtt.client as mqtt
+import paho.mqtt.publish as publish
 
-_g_cst_ToMQTTTopicServerIP = "localhost"
-_g_cst_ToMQTTTopicServerPort = 1883 #port
-_g_cst_MQTTTopicName = "MYTOPIC" #TOPIC name
+MQTTServerIP = "localhost"
+MQTTServerPort = 1883 #port
+MQTTTopicName = "MYTOPIC" #TOPIC name
 
 mqttc = mqtt.Client("python_pub")
-mqttc.connect(_g_cst_ToMQTTTopicServerIP, _g_cst_ToMQTTTopicServerPort)
-mqttc.publish(_g_cst_MQTTTopicName, "Hello")
+mqttc.connect(MQTTServerIP, MQTTServerPort)
+mqttc.publish(MQTTTopicName, "1")
+publish.single(MQTTTopicName,"2")
--- mqtt/Subscriber.py
+++ mqtt/Subscriber.py
@@ -6,10 +6,10 @@ def on_connect(client, userdata, flags, rc):
     client.subscribe("MYTOPIC")
 
 def on_message(client, userdata, msg):
-    print(msg.topic+" "+str(msg.payload))
+    print(msg.topic+" "+str(msg.payload)+" "+str(msg.qos))
 
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-client.connect("localhost", 1883, 60)
+client.connect("localhost", 1883, 60,str(2))
 client.loop_forever()
